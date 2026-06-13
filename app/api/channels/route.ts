10c2ee7a6ac4c2fe8a89efd24458481196e885a7
Sudhev <69240835+SKS-WEBDEV@users.noreply.github.com>
2026-06-14 00:50:39 +0530

    Update route.ts

--- app/api/channels/route.ts
+++ app/api/channels/route.ts
@@ -13,6 +13,24 @@ const KNOWN_CHANNELS: Channel[] = [
     group: 'FIFA World Cup 2026',
   },
   {
+    name: 'FOX Sports 1 (1280p)',
+    url: 'https://cors-proxy.cooks.fyi/http://190.11.225.124:5000/live/fs1_hd/playlist.m3u8',
+    logo: 'https://i.imgur.com/O9BapV9.png',
+    group: 'Sports',
+  },
+  {
+    name: 'FOX Sports 2 (480p)',
+    url: 'https://tvsen7.aynaott.com/foxsports2/index.m3u8',
+    logo: 'https://i.imgur.com/LHtxKI8.png',
+    group: 'Sports',
+  },
+  {
+    name: 'FOX Sports (720p)',
+    url: 'https://jmp2.uk/plu-5a74b8e1e22a61737979c6bf.m3u8',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/FOX_Sports_logo.svg/960px-FOX_Sports_logo.svg.png',
+    group: 'Sports',
+  },
+  {
     name: 'Sports Grid',
     url: 'https://d35j504z0x2vu2.cloudfront.net/v1/master/0bc8e8376bd8417a1b6761138aa41c26c7309312/sportsgrid/master.m3u8',
     logo: '',
